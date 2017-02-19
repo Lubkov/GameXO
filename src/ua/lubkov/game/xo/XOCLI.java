7e7a3f2f05a38658dac0accffa0e103bf87583cd
Lubkov <ser.lubkov@gmail.com>
2017-02-19 23:03:34 +0200

    XOCLI - добавлен ввод имени игроков

--- src/ua/lubkov/game/xo/XOCLI.java
+++ src/ua/lubkov/game/xo/XOCLI.java
@@ -10,19 +10,18 @@ import ua.lubkov.game.xo.view.ConsoleView;
 public class XOCLI {
 
     private static final int PLAYER_COUNT = 2;
+    private static final int FIELD_SIZE = 3;
 
     public static void main(final String[] arg) {
 
-        final String name1 = "Сергей";
-        final String name2 = "Вероника";
-
+        ConsoleView consoleView = new ConsoleView();
+        final String name1 = consoleView.askPlayerName(1);
+        final String name2 = consoleView.askPlayerName(2);
         final Player[] players = new Player[PLAYER_COUNT];
         players[0] = new Player(name1, Figure.X);
         players[1] = new Player(name2, Figure.O);
 
-        Game game = new Game(players, new Field(3), "X&O");
-
-        ConsoleView consoleView = new ConsoleView();
+        Game game = new Game(players, new Field(FIELD_SIZE), "X&O");
         consoleView.show(game);
 
         while (consoleView.step(game)) {
